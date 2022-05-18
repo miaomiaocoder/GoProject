90301931e56f668c4ae26aabe8c127dc85f97224
renzhenhua <2452503780@qq.com>
2022-05-18 11:08:56 +0800

    通过 Go 语言学习测试驱动开发

---
+++ LearnGoWithTests/hello/hello.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	/* 这是我的第一个简单的程序 */
+	fmt.Println(Hello("World", ""))
+}
