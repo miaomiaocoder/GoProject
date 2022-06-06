38eeed50f368f37c52a5eb80376bf90b4ca52e4b
renzhenhua <2452503780@qq.com>
2022-06-06 16:27:27 +0800

    完成数据，切片的驱动测试开发

--- LearnGoWithTests/array/Sum.go
+++ LearnGoWithTests/array/Sum.go
@@ -7,3 +7,17 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			var tail []int = numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- LearnGoWithTests/array/sum_test.go
+++ LearnGoWithTests/array/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -14,15 +17,27 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
 
-	t.Run("clollection of 5 numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
+func TestSumAllTails(t *testing.T) {
 
-		got := Sum(numbers)
-		want := 6
-
-		if want != got {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 3, 2}, []int{0, 9})
+		want := []int{5, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
