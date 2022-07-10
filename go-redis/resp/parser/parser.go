a64729397e4b105aad77fccf244a1b2aa0c9b3df
renzhenhua <2452503780@qq.com>
2022-07-10 21:16:22 +0800

    实现ParseStream, ReadLine, ParseMultiBulkHeader, ParseSingleLine, readBody

---
+++ go-redis/resp/parser/parser.go
@@ -0,0 +1,147 @@
+package parser
+
+import (
+	"bufio"
+	"errors"
+	"go-redis/interface/resp"
+	"go-redis/resp/reply"
+	"io"
+	"strconv"
+	"strings"
+)
+
+type Payload struct {
+	Data resp.Reply
+	Err  error
+}
+
+type readState struct {
+	readingMultiLine  bool
+	expectedArgsCount int
+	msgType           byte
+	args              [][]byte
+	bulkLen           int64
+}
+
+func (s *readState) finished() bool {
+	return s.expectedArgsCount > 0 && len(s.args) == s.expectedArgsCount
+}
+
+func ParseStream(reader io.Reader) <-chan *Payload {
+	ch := make(chan *Payload)
+	go parse0(reader, ch)
+	return ch
+}
+
+func parse0(reader io.Reader, ch chan<- *Payload) {
+
+}
+
+// *3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n
+func readLine(bufReader *bufio.Reader, state *readState) ([]byte, bool, error) {
+	var msg []byte
+	var err error
+	if state.bulkLen == 0 { //1.\r\n切分
+		msg, err = bufReader.ReadBytes('\n')
+		if err != nil {
+			return nil, true, err
+		}
+		if len(msg) == 0 || msg[len(msg)-2] != '\r' {
+			return nil, false, errors.New("protocol error:" + string(msg))
+		}
+	} else { //2.之前读到了$数字，严格读取字符个数
+		msg = make([]byte, state.bulkLen+2)
+		_, err = io.ReadFull(bufReader, msg)
+		if err != nil {
+			return nil, true, err
+		}
+		if len(msg) == 0 || msg[len(msg)-2] != '\r' || msg[len(msg)-1] != '\n' {
+			return nil, false, errors.New("protocol error:" + string(msg))
+		}
+		state.bulkLen = 0
+	}
+	return msg, false, nil
+}
+
+// *3\r\n$3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n
+func parseMultiBulkHeader(msg []byte, state *readState) error {
+	var err error
+	var expectedLine uint64
+	expectedLine, err = strconv.ParseUint(string(msg[1:len(msg)-2]), 10, 32)
+	if err != nil {
+		return errors.New("protocol error:" + string(msg))
+	}
+	if expectedLine == 0 {
+		state.expectedArgsCount = 0
+		return nil
+	} else if expectedLine > 0 {
+		state.msgType = msg[0]
+		state.readingMultiLine = true
+		state.expectedArgsCount = int(expectedLine)
+		state.args = make([][]byte, 0, expectedLine)
+		return nil
+	} else {
+		return errors.New("protocol error:" + string(msg))
+	}
+}
+
+// $4\r\nPING\r\n
+func parseBulkHeader(msg []byte, state *readState) error {
+	var err error
+	state.bulkLen, err = strconv.ParseInt(string(msg[1:len(msg)-2]), 10, 64)
+	if err != nil {
+		return errors.New("protocol error:" + string(msg))
+	}
+	if state.bulkLen == -1 {
+		return nil
+	} else if state.bulkLen > 0 {
+		state.msgType = msg[0]
+		state.readingMultiLine = true
+		state.expectedArgsCount = 1
+		state.args = make([][]byte, 0, 1)
+		return nil
+	} else {
+		return errors.New("protocol error:" + string(msg))
+	}
+}
+
+// +OK\r\n -err\r\n :5\r\n
+func parseSingleLineReply(msg []byte) (resp.Reply, error) {
+	str := strings.TrimSuffix(string(msg), "\r\n")
+	var result resp.Reply
+	switch msg[0] {
+	case '+':
+		result = reply.MakeStatusReply(str[1:])
+	case '-':
+		result = reply.MakeErrReply(str[1:])
+	case ':':
+		val, err := strconv.ParseInt(str[1:], 10, 64)
+		if err != nil {
+			return nil, errors.New("protocol error:" + string(msg))
+		}
+		result = reply.MakeIntReply(val)
+	}
+	return result, nil
+}
+
+// $3\r\nSET\r\n$3\r\nkey\r\n$5\r\nvalue\r\n
+// PING\r\n
+func readBody(msg []byte, state *readState) error {
+	line := msg[0 : len(msg)-2]
+	var err error
+	//$3
+	if line[0] == '$' {
+		state.bulkLen, err = strconv.ParseInt(string(line[1:]), 10, 64)
+		if err != nil {
+			return errors.New("protocol error:" + string(msg))
+		}
+		//$0\r\n
+		if state.bulkLen <= 0 {
+			state.args = append(state.args, []byte{})
+			state.bulkLen = 0
+		}
+	} else {
+		state.args = append(state.args, line)
+	}
+	return nil
+}
